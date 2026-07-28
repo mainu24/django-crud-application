a46c4cbdc8a8e7702f360676b484c73a41422c55
mainu24 <patelmainu1@gmail.com>
2026-07-28 15:00:30 +0530

    Adjust Django settings for production
    Updated settings for production environment including setting DEBUG to False and modifying ALLOWED_HOSTS.

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pm3na#y&(6arm#z&mbk#^71u(*-#_rr(hzmq@i+)v@7h5kv@hb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
